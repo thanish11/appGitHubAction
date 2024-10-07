211fe306d8fff1051dc0d4f79f21f03351ddee50
Thanish S <71495782+thanish11@users.noreply.github.com>
2024-10-07 19:20:28 +0530

    Update test_operations.py

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,12 +1,12 @@
 from src.math_operations import add,sub
 
 #unit test
-def add_test():
+def test_add():
     assert add(2,3)==5
     assert add(-1,1)==0
     assert add(2,-1)==1
 
-def sub_test():
+def test_sub():
     assert sub(5,3)==2
     assert sub(4,3)==1
     assert sub(-1,1)==-2
